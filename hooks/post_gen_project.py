5b027c1a9a5db5426ff0f1e69cf7668c1e80dfb1
Zeheng Li <imzehengl@gmail.com>
2021-07-28 14:04:08 -0600

    update template

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,8 @@
 import subprocess
 
+
+message = "initial commit from gh:zehengl/cookiecutter-conda-script"
+
 subprocess.call(["git", "init"])
 subprocess.call(["git", "add", "*"])
-subprocess.call(
-    ["git", "commit", "-m", "initial commit from gh:zehengl/cookiecutter-conda-script"]
-)
+subprocess.call(["git", "commit", "-m", message])

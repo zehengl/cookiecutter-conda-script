29cf7dddcbf2a409bdf63e7f11a84572e497778f
Zeheng Li <imzehengl@gmail.com>
2021-09-09 09:16:22 -0600

    allow tests

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,10 +3,14 @@ import shutil
 import subprocess
 
 
+folder = os.getcwd()
+
 if not "{{cookiecutter.writing_docs}}" == "yes":
-    folder = os.getcwd()
     shutil.rmtree(os.path.join(folder, "docs"))
     os.remove(os.path.join(folder, "mkdocs.yml"))
+
+if not "{{cookiecutter.writing_tests}}" == "yes":
+    shutil.rmtree(os.path.join(folder, "tests"))
 
 message = "initial commit from gh:zehengl/cookiecutter-conda-script"
 
